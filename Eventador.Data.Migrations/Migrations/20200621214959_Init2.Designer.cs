bb2c20217dfd547dd8580469e277ff85da18d3fc
a.barkin <a.m.barkin@gmail.com>
2020-06-22 00:50:28 +0300

    migration 2

--- Eventador.Data.Migrations/Migrations/20200621214959_Init2.Designer.cs
+++ Eventador.Data.Migrations/Migrations/20200621214959_Init2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Eventador.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Eventador.Data.Migrations.Migrations
 {
     [DbContext(typeof(EventadorDbContext))]
-    partial class EventadorDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200621214959_Init2")]
+    partial class Init2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Eventador.Data.Migrations/Migrations/20200621214959_Init2.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eventador.Data.Migrations.Migrations
+{
+    public partial class Init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "user_type",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "additional_info",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "event_status",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "event_type",
+                table: "events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "author_id",
+                table: "events",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "status",
+                table: "events",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "type",
+                table: "events",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "status",
+                table: "events");
+
+            migrationBuilder.DropColumn(
+                name: "type",
+                table: "events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "user_type",
+                table: "users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "author_id",
+                table: "events",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddColumn<string>(
+                name: "additional_info",
+                table: "events",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "event_status",
+                table: "events",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "event_type",
+                table: "events",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
